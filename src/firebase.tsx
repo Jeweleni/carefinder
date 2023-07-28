e89b40ef8c29735d1dc483e83bc56fc57850b90a
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-07-28 22:26:39 +0100

    google

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,24 +1,25 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import {
-  getAuth,
-  signInWithPopup,
-  createUserWithEmailAndPassword,
-  GoogleAuthProvider,
-} from "firebase/auth";
+import { getAuth, signInWithPopup, createUserWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics, logEvent } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCYFXg4WfKJYwlD0lUOALpJksSKYHDYx-I",
-  authDomain: "carefinder-68614.firebaseapp.com",
-  projectId: "carefinder-68614",
-  storageBucket: "carefinder-68614.appspot.com",
-  messagingSenderId: "433599252416",
-  appId: "1:433599252416:web:a43ffb15a78b8fb7fcea25",
-  measurementId: "G-W3C0FWXDCZ",
+  apiKey: "AIzaSyDR4Ig2_MYzca769cgpxqqHHhU_eQLAA5w",
+  authDomain: "carefinder-f97bb.firebaseapp.com",
+  projectId: "carefinder-f97bb",
+  storageBucket: "carefinder-f97bb.appspot.com",
+  messagingSenderId: "405540239833",
+  appId: "1:405540239833:web:fb545855e8af6a01d241c8",
+  measurementId: "G-2LMQNT2GSP"
 };
 
+// Initialize Firebase
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
